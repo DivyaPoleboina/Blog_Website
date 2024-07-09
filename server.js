c217be3afc3f13044f2f009a634957204872a31b
DivyaPoleboina <divyapoleboina@gmail.com>
2024-07-09 20:08:11 +0530

    web application

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const articleRouter = require("./routes/articles")
+const Article = require('./models/article')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const app = express()
+
+mongoose.connect('enter your mongodb link')
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
+app.set("views", "./view")
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+app.get('/', async(req, res) => {
+    const articles =await Article.find().sort({ createdAt:'desc'})
+    res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(3000,()=>{
+    console.log("server is running on 3000")
+})
